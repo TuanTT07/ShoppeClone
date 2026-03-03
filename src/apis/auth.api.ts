6fc116a5e29864eff84d8fbca47460e4031dff7b
TuanTran <160764958+TuanTran0707@users.noreply.github.com>
2026-03-03 16:42:55 +0700

    feat: add react-toastify for notification handling and improve error response feedback

--- src/apis/auth.api.ts
+++ src/apis/auth.api.ts
@@ -2,5 +2,5 @@ import type { AuthResponse } from '../types/auth.type';
 import http from '../utils/http';
 
 export const resgisterAccount = (body: { email: string; password: string }) => {
-  return http.post<AuthResponse>('/register', body);
+  return http.post<AuthResponse>('/registerr', body);
 };
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,5 @@
-import axios, { type AxiosInstance } from 'axios';
+import axios, { AxiosError, type AxiosInstance } from 'axios';
+import { toast } from 'react-toastify';
 class Http {
   instance: AxiosInstance;
   constructor() {
@@ -9,6 +10,20 @@ class Http {
         'Content-Type': 'application/json',
       },
     });
+    this.instance.interceptors.response.use(
+      function (response) {
+        return response;
+      },
+      function (error: AxiosError) {
+        if (error.response?.status !== 422) {
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const data: any | undefined = error.response?.data;
+          const message = data.message || error.message;
+          toast.error(message);
+        }
+        return Promise.reject(error);
+      }
+    );
   }
 }
 const http = new Http().instance;
